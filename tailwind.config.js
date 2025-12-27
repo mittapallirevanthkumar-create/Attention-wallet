3c597e9d0620e36c3b389ef943e171c93016e845
mittapallirevanthkumar-create <mittapallirevanthkumar@gmail.com>
2025-12-27 13:04:38 +0530

    Create  tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+```
+
+### 8. **.gitignore**
+```
+# dependencies
+/node_modules
+/.pnp
+.pnp.js
+
+# testing
+/coverage
+
+# production
+/build
+
+# misc
+.DS_Store
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
